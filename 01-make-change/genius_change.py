c0caefcebae6c9913707db42e267134fc97fe172
geniusmehul <34449205+geniusmehul@users.noreply.github.com>
2018-10-05 14:36:06 +0530

    Updated to ensure over change

--- 01-make-change/genius_change.py
+++ 01-make-change/genius_change.py
@@ -1,4 +1,6 @@
 def change(amt, coins):
+  _amt = amt
+  _coins = [c for c in coins]
   coins_dict = {c: 0 for c in coins}
   coins.sort(reverse=1)
   for coin in coins:
@@ -6,7 +8,12 @@ def change(amt, coins):
     amt = amt // coin
     coins_dict[coin] = amt
     amt = holding_amt % coin
-  return list(coins_dict.values())
+  if amt != 0:
+    return change(_amt-amt+coins[-1], _coins)
+  else:
+    return list(coins_dict.values())
   
 print(change (83, [25, 10, 5, 1]))  # [3,0,1,3]
 print(change (83, [25, 1, 5, 10]))  # [3,3,1,0]
+print(change (83, [25, 10, 5]))  # [3,1,0]
+print(change (98, [10, 25, 5]))  # [0,4,0]
